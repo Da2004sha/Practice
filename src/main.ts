8ce0d18c64440ff990e156e40405631d02ef3f9a
maximmikhailov1 <max.mikhailov2003@yandex.ru>
2024-06-28 18:20:33 +0300

    check2

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-// Создание приложения
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
